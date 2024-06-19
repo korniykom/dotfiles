4272d804611e4b29acff4df7f1cb365d1ffcdbb1
Max <korniykom@gmail.com>
2024-06-19 17:53:49 +0300

    fead: add desctop config

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-cp ./hyprland/hyprland.conf ~/.config/hypr/
-cp ./kitty ./wofi ./hyprlock ~/.config/
-cp ./waybar/style.css config /etc/xdg/waybar
+cp ./hyprland/hyprland.conf /home/max/.config/hypr
+cp ./hyprlock/hyprlock.conf /home/max/.config/hypr
+cp -r ./kitty /home/max/.config/
+cp -r ./wofi /home/max/.config/
+cp ./hyprpaper/hyprpaper.conf /home/max/.config/hypr
+
+
+cp ./waybar/style.css ./waybar/config /etc/xdg/waybar
+cp ./waybar/config /etc/xdg/waybar
