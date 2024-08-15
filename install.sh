6154dbc66fd67a364ba942a489493dc4cba5a26b
Maksym Korniiko <korniykom@gmail.com>
2024-08-15 23:09:36 +0300

    fix: apply wallpaper at hdmi port

--- install.sh
+++ install.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-pacman -Syu hyprland kitty firefox waybar wofi thunar kitty hyprpaper hyprlock brightnessctl pamixer ttf-font-awesome ttf-roboto 
-echo Install hyprshot via aur
 cp ./hyprland/hyprland.conf /home/max/.config/hypr
 cp ./hyprlock/hyprlock.conf /home/max/.config/hypr
 cp -r ./kitty /home/max/.config/
@@ -11,3 +9,6 @@ cp ./hyprpaper/wallpaper.png /home/max/.config/hypr
 
 cp ./waybar/style.css ./waybar/config /etc/xdg/waybar
 cp ./waybar/config /etc/xdg/waybar
+
+pacman -Syu hyprland kitty firefox waybar wofi thunar kitty hyprpaper hyprlock brightnessctl pamixer ttf-font-awesome ttf-roboto 
+echo Install hyprshot via aur
