35c7925c44bf0f3716a05add2dd9077bb9d4cb46
Max <korniykom@gmail.com>
2025-05-17 12:11:24 +0300

    chore: replate firefox with zen-browser and kitty with ghostty

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# pacman -Syu hyprland kitty firefox waybar wofi thunar kitty hyprpaper hyprlock brightnessctl pamixer ttf-font-awesome ttf-roboto
+ pacman -Syu hyprland ghostty firefox waybar wofi thunar kitty hyprpaper hyprlock brightnessctl pamixer ttf-font-awesome ttf-roboto
 cp ./hyprland/hyprland.conf /home/max/.config/hypr
 cp ./hyprlock/hyprlock.conf /home/max/.config/hypr
 cp -r ./kitty /home/max/.config/
