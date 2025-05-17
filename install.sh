fb06be839095cbe541f85d3327a18b70c46beab9
Max <korniykom@gmail.com>
2025-05-17 13:31:50 +0300

    new dotfiles

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
- #pacman -Syu hyprland ghostty firefox waybar wofi thunar  hyprpaper hyprlock brightnessctl pamixer ttf-font-awesome ttf-roboto
+ #pacman -Syu hyprland ghostty firefox waybar wofi thunar  hyprpaper hyprlock brightnessctl pamixer ttf-font-awesome ttf-roboto 
 cp ./hyprland/hyprland.conf /home/max/.config/hypr
 cp ./hyprlock/hyprlock.conf /home/max/.config/hypr
 cp -r ./wofi /home/max/.config/
 cp ./hyprpaper/hyprpaper.conf /home/max/.config/hypr
 cp -r ./hyprpaper/wallpaper /home/max/.config/hypr
+cp ./ghostty/config /home/max/.config/ghostty
 
-
-cp ./waybar/style.css ./waybar/config /etc/xdg/waybar
-cp ./waybar/config /etc/xdg/waybar
+cp ./waybar/style.css  /etc/xdg/waybar/style.css
+cp ./waybar/config /etc/xdg/waybar/config
