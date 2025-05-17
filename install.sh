9f4c951de1850f92deb32a2764f0a4411e4bbee2
Max <korniykom@gmail.com>
2025-05-17 12:27:45 +0300

    feat: update wallpaper to cyberpunk1

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
- pacman -Syu hyprland ghostty firefox waybar wofi thunar kitty hyprpaper hyprlock brightnessctl pamixer ttf-font-awesome ttf-roboto
+ #pacman -Syu hyprland ghostty firefox waybar wofi thunar  hyprpaper hyprlock brightnessctl pamixer ttf-font-awesome ttf-roboto
 cp ./hyprland/hyprland.conf /home/max/.config/hypr
 cp ./hyprlock/hyprlock.conf /home/max/.config/hypr
-cp -r ./kitty /home/max/.config/
 cp -r ./wofi /home/max/.config/
 cp ./hyprpaper/hyprpaper.conf /home/max/.config/hypr
 cp -r ./hyprpaper/wallpaper /home/max/.config/hypr
